d33f5a579f8c157347b97919bb202c466f95b415
Stefan Kreitmayer <stefankreitmayer@gmail.com>
2020-11-22 09:23:44 +0000

    send post request to control the motors

--- lib/rover_web/live/page_live.ex
+++ lib/rover_web/live/page_live.ex
@@ -1,9 +1,11 @@
 defmodule RoverWeb.PageLive do
   use RoverWeb, :live_view
 
+  @api_root "morty.local:5000/api/v1"
+
   @impl true
   def mount(_params, _session, socket) do
-    Phoenix.PubSub.subscribe(Rover.PubSub, "photo_upload") # deprecated
+    # Phoenix.PubSub.subscribe(Rover.PubSub, "photo_upload") # deprecated
     {:ok, socket |> fetch_photo()}
   end
 
@@ -28,8 +30,8 @@ defmodule RoverWeb.PageLive do
   end
 
   @impl true
-  def handle_info(:stop_motors, socket) do
-    {:noreply, socket |> assign(movement: nil)}
+  def handle_info(:movement_complete, socket) do
+    {:noreply, socket |> assign(motor_feedback: nil) |> fetch_photo()}
   end
 
   @impl true
@@ -37,13 +39,38 @@ defmodule RoverWeb.PageLive do
     {:noreply, socket |> assign(photo: image)}
   end
 
-  def move(socket, left, right) do
-    if socket.assigns[:movement] do
+  defp move(socket, left, right) do
+    duration = 300
+    left = left * 0.3
+    right = right * 0.3
+    if socket.assigns[:motor_feedback] do
       socket
     else
-      Process.send_after(self(), :stop_motors, 1000)
+      Process.send_after(self(), :movement_complete, duration)
+      message =
+        case request_robot_move(left, right, duration) do
+          :ok ->
+            "#{left}  #{right}"
+          {:error, message} ->
+            message
+        end
       socket
-      |> assign(:movement, "#{left}  #{right}")
+      |> assign(:motor_feedback, message)
+    end
+  end
+
+  defp request_robot_move(left, right, duration) do
+    url = "#{@api_root}/move"
+    try do
+      body = %{"left_speed" => left, "right_speed" => right, "duration" => duration}
+      json = Jason.encode!(body)
+      response = HTTPoison.post!(url, json, [{"Content-Type", "application/json"}])
+      case response.status_code do
+        200 -> :ok
+        _ -> {:error, "HTTP response code: #{response.status_code}"}
+      end
+    rescue
+      e -> {:error, "Error: #{e.reason}"}
     end
   end
 
@@ -53,7 +80,7 @@ defmodule RoverWeb.PageLive do
   end
 
   defp fetch_photo_html do
-    url = "morty.local:5000/api/v1/snap"
+    url = "#{@api_root}/snap"
     try do
       response = HTTPoison.get!(url)
       data = Jason.decode!(response.body)
@@ -62,6 +89,5 @@ defmodule RoverWeb.PageLive do
     rescue
       e -> {:error, "Error: #{e.reason}"}
     end
-
   end
 end
